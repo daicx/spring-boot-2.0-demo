fdd65588aaceba0b72ff4ffbdbc8374232baac88
dcx <807404400@qq.com>
2019-10-30 23:40:42 +0800

    ignore

--- freemaker/src/main/java/com/example/freemaker/controller/UserController.java
+++ freemaker/src/main/java/com/example/freemaker/controller/UserController.java
@@ -1,13 +1,24 @@
 package com.example.freemaker.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Locale;
+
 @Controller
 public class UserController {
+
+    @Autowired
+    private MessageSource messageSource;
+
     @GetMapping("/user")
     public String user(ModelMap modelMap) {
+        Locale locale = LocaleContextHolder.getLocale();
+        modelMap.addAttribute("well", messageSource.getMessage("message", null, locale));
         modelMap.addAttribute("name", "小张");
         return "index";
     }
--- freemaker/src/main/resources/templates/index.ftl
+++ freemaker/src/main/resources/templates/index.ftl
@@ -5,6 +5,8 @@
     <title>首页</title>
 </head>
 <body>
-${name}
+<a href="/user?lang=en_US">English</a>
+<a href="/user?lang=zh_CN">中文</a>
+${well}--${name}
 </body>
 </html>
